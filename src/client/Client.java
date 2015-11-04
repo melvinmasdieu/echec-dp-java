b0233e60b13f1fc7baaa2de52d1a4f0d43f2cdfa
Thomas <thomashenry@hotmail.fr>
2015-11-04 17:09:13 +0100

    client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,7 +1,51 @@
 package client;
 
+
+import controler.controlerLocal.ChessGameControler;
+import model.ChessGame;
+import vue.ChessGameGUI;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Created by melvin on 04/11/15.
  */
 public class Client {
+    public static Socket socket = null;
+    public static Thread t1,t2;
+
+
+
+    public static void main(String[] args) {
+
+
+        try {
+
+            System.out.println("Demande de connexion au serveur");
+            socket = new Socket("127.0.0.1",2009);
+            System.out.println("Connexion établie avec le serveur, authentification :"); // Si le message s'affiche c'est que je suis connecté
+
+
+
+            t1 = new Thread(new ClientOut(socket,));   // thread d'émission
+            t1.start();
+            t2 = new Thread(new ClientIn(socket));    // thread de réception
+            t2.start();
+
+            ChessGame chessGame = new ChessGame();
+            ChessGameControler clientControler = new ChessGameControler(chessGame);
+
+            ChessGameGUI chessGameGUI = new ChessGameGUI(clientControler);
+
+        } catch (UnknownHostException e) {
+            System.err.println("Impossible de se connecter à l'adresse "+socket.getLocalAddress());
+        } catch (IOException e) {
+            System.err.println("Aucun serveur à l'écoute du port "+socket.getLocalPort());
+        }
+
+
+
+    }
 }
